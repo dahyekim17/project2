c2fde2b7a1f4b4990211bc25dba839a5df20d6c4
dahyekim17 <110885336+dahyekim17@users.noreply.github.com>
2022-08-16 12:17:51 +0900

    Add files via upload

---
+++ js/topbtn.js
@@ -0,0 +1,14 @@
+const btt = document.querySelector("#back-to-top");
+let scrollAmount;
+window.addEventListener("scroll", function () {
+  scrollAmount = this.scrollY;
+  if (scrollAmount > scrollAmount / 4) {
+    btt.classList.add("visible");
+  } else {
+    btt.classList.remove("visible");
+  }
+});
+btt.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(btt.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+});
